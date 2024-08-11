25c5fd087007c097dc4a23603ef6b00f9b4807e9
Milton Muñoz <livemilton@gmail.com>
2024-08-10 19:44:17 -0500

    first commit and cards logic source development

---
+++ src/main/java/com/livemilton/cards/CardsApplication.java
@@ -0,0 +1,42 @@
+package com.livemilton.cards;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.cards.controller") })
+@EnableJpaRepositories("com.eazybytes.cards.repository")
+@EntityScan("com.eazybytes.cards.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "livemiltonBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Milton Munoz",
+						email = "livemilton@hotmail.com",
+						url = "https://www.livemilton.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.livemilton.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "livemiltonBank Cards microservice REST API Documentation",
+				url = "https://www.livemilton.com/swagger-ui.html"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+}
