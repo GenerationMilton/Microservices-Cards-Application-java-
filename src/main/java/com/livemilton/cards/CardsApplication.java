2292fd271657c545d8b58e0fb37c8cfbceb361c2
Milton Muñoz <livemilton@gmail.com>
2024-08-30 21:07:07 -0500

    about docker path and spring profiles using yml qa and prod

--- src/main/java/com/livemilton/cards/CardsApplication.java
+++ src/main/java/com/livemilton/cards/CardsApplication.java
@@ -1,39 +1,29 @@
 package com.livemilton.cards;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.livemilton.cards.dto.CardsContactInfoDto;
+
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-/*@ComponentScans({ @ComponentScan("com.eazybytes.cards.controller") })
-@EnableJpaRepositories("com.eazybytes.cards.repository")
-@EntityScan("com.eazybytes.cards.model")*/
+/*
+ * @ComponentScans({ @ComponentScan("com.eazybytes.cards.controller") })
+ * 
+ * @EnableJpaRepositories("com.eazybytes.cards.repository")
+ * 
+ * @EntityScan("com.eazybytes.cards.model")
+ */
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Cards microservice REST API Documentation",
-				description = "livemiltonBank Cards microservice REST API Documentation",
-				version = "v1",
-				contact = @Contact(
-						name = "Milton Munoz",
-						email = "livemilton@hotmail.com",
-						url = "https://www.livemilton.com"
-				),
-				license = @License(
-						name = "Apache 2.0",
-						url = "https://www.livemilton.com"
-				)
-		),
-		externalDocs = @ExternalDocumentation(
-				description = "livemiltonBank Cards microservice REST API Documentation",
-				url = "https://www.livemilton.com/swagger-ui.html"
-		)
-)
+@EnableConfigurationProperties(value = { CardsContactInfoDto.class })
+@OpenAPIDefinition(info = @Info(title = "Cards microservice REST API Documentation", description = "livemiltonBank Cards microservice REST API Documentation", version = "v1", contact = @Contact(name = "Milton Munoz", email = "livemilton@hotmail.com", url = "https://www.livemilton.com"), license = @License(name = "Apache 2.0", url = "https://www.livemilton.com")), externalDocs = @ExternalDocumentation(description = "livemiltonBank Cards microservice REST API Documentation", url = "https://www.livemilton.com/swagger-ui.html"))
 public class CardsApplication {
 
 	public static void main(String[] args) {
